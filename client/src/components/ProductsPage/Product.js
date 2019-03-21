6c18e4f9da0752d6fdb6bbeef59a0750ab41e3d2
leekhanh1993 <lkgame1993@gmail.com>
2019-03-22 01:10:21 +0700

    commit

--- client/src/components/ProductsPage/Product.js
+++ client/src/components/ProductsPage/Product.js
@@ -3,15 +3,14 @@ import React from 'react'
 function Product(Props) {
     const {product} = Props;
     return (
-        <div>
-            <div className="card mb-3" style={{ maxWidth: 540 }}>
+            <div className="card product mx-auto mb-5">
                 <div className="row no-gutters">
                     <div className="col-md-4">
                         <img src={product.imageURL} className="card-img" alt={product.name} />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">{product.name}</h5>
+                            <h5 className="card-title text-truncate">{product.name}</h5>
                             <p className="text-danger">{product.price} $</p>
                             <p className="card-text">Độ dày: {product.thickness}</p>
                             <p className="card-text">Chất liệu: {product.material}</p>
@@ -22,7 +21,6 @@ function Product(Props) {
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
